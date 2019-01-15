69bf3de0ea9c5fb0a9976e2e49a9a661a5e8ecf5
DR <danielrubak@gmail.com>
2019-01-15 23:40:26 +0100

    Adds some solutions to exam tasks from last years

---
+++ test/test-solutions.pl
@@ -0,0 +1,33 @@
+dodaj_jeden([],[]).
+
+dodaj_jeden([X|List], [X2|List2]) :-
+    dodaj_jeden(List, List2),
+    X2 is X + 1.
+
+% ------------------------------------------------ %
+
+podziel([], [], []).
+
+podziel([Head|Tail], [Head|List1], List2) :-
+   Head>=0,
+   podziel(Tail, List1, List2).
+
+podziel([Head|Tail], List1, [Head|List2]) :-
+   Head<0,
+   podziel(Tail, List1, List2).
+
+% ------------------------------------------------ %
+
+umiesc(Element, List, 0, [Element|List]).
+
+umiesc(Element, [E|List], Position, [E|RL]):-
+    Pos1 is Position-1,
+    umiesc(Element, List, Pos1, RL).
+
+% ------------------------------------------------ %
+
+zakres(Stop, Stop, [Stop]).
+
+zakres(Start, Stop, [Start|RL]) :-
+   Start1 is Start + 1,
+   zakres(Start1, Stop, RL).
